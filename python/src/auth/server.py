698975625c2f2d8d540e555099f874ac9243fe8d
zaidsaeed <zsaee060@uottawa.ca>
2023-08-03 18:54:21 -0700

    (auth): finished auth service code, need dockerfile next

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -38,6 +38,24 @@ def login():
         return "Invalid credentials", 401
 
 
+@server.route("/validate", method=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+
+    if not encoded_jwt:
+        return "missing credentials", 401
+    
+    encoded_jwt = encoded_jwt.split(" ")[1]
+
+    try:
+        decoded = jwt.decode(
+            encoded_jwt, os.environ.get("JWT_SECRET"), algorithm=["HS256"]
+        )
+    except:
+        return "not authorized", 403
+    
+    return decoded, 200
+
 def createJWT(username, secret, authz):
     return jwt.encode(
         {
@@ -52,4 +70,3 @@ def createJWT(username, secret, authz):
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=5000)
-    
