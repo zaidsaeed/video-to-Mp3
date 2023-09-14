6f18630be6e9d7b16c91fadcf93f3b4e2fe76e5e
zaidsaeed <zsaee060@uottawa.ca>
2023-09-14 10:01:32 -0700

    created a workspace with each subdirectory as a root directory

---
+++ auth/init.sql
@@ -0,0 +1,15 @@
+CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth123';
+
+CREATE DATABASE auth;
+
+GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
+
+USE auth;
+
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+INSERT into user(email, password) VALUES ('saeedzaid003@gmail.com', 'Admin123');
